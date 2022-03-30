cc3b2725fe28a4169f64e0eefd415e8b9a91103c
hussnain-imtiaz <hussnainimtiaz0@gmail.com>
2022-03-30 19:57:14 +0000

    week1-4

---
+++ app.py
@@ -0,0 +1,6 @@
+def just_looking_in(name):
+    return name.upper()
+
+
+username = "hussnain"
+print(f"Hello {username}, your name in upper case will be {just_looking_in(username)}")
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import just_looking_in
+
+
+def test_just_looking_in():
+    assert just_looking_in("hussnain") == "HUSSNAIN"
