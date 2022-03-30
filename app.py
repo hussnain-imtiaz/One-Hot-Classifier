62616234fc31cbca359a115f4dbbcae09157b046
hussnain-imtiaz <hussnainimtiaz0@gmail.com>
2022-03-30 20:22:12 +0000

    error-solved

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ def just_looking_in(name):
     return name.upper()
 
 
-username = "hussnain
+username = "hussnain"
 print(f"Hello {username}, your name in upper case will be {just_looking_in(username)}")
